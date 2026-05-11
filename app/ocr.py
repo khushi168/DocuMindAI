be7fe5782a21ca9cf598b71d04343adeede3606a
khushi168 <khushibatra168@gmail.com>
2026-05-11 21:30:22 +0530

    Render deplyment setup

--- app/ocr.py
+++ app/ocr.py
@@ -1,10 +1,5 @@
-from PIL import Image
-import pytesseract
+def extract_text_from_image(filepath):
 
-def extract_text_from_image(image_path):
-
-    image = Image.open(image_path)
-
-    text = pytesseract.image_to_string(image)
-
-    return text
+    return (
+        "OCR is disabled in deployed version."
+    )
